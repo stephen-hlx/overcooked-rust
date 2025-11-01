28cd35f7dc532000331c6fc0065b7b10ccf47931
Stephen Wong <stephen.hlx+github@gmail.com>
2025-11-01 12:58:21 -0700

    Add example of using Action
    But it may not support more than 2 actor implementations ..

--- core/src/action.rs
+++ core/src/action.rs
@@ -2,16 +2,18 @@ use std::{error::Error, pin::Pin, sync::Arc};
 
 use crate::actor::ActorBase;
 
-pub struct Action<T1, T2>
+/// We may need to replace T1 with Box<T1> just to make sure
+/// we can support more than 2 `ActorBase` implementations.
+pub struct ActionDefinition<T1, T2>
 where
     T1: ActorBase,
     T2: ActorBase,
 {
     pub label: &'static str,
-    pub action_type: ActionType<T1, T2>,
+    pub action: Action<T1, T2>,
 }
 
-pub enum ActionType<T1, T2>
+pub enum Action<T1, T2>
 where
     T1: ActorBase,
     T2: ActorBase,
@@ -43,82 +45,71 @@ impl ActorBase for DummyActionReceiver {}
 
 #[cfg(test)]
 mod tests {
-    use std::{error::Error, pin::Pin, sync::Arc};
+    use std::{error::Error, sync::Arc};
 
     use crate::{
-        action::{Action, ActionType, DummyActionReceiver},
+        action::{Action, DummyActionReceiver},
         actor::ActorBase,
         test_utils::test_actor_states::{TestActor1, TestActor2},
     };
 
     #[tokio::test]
-    async fn action_type_can_be_defined_and_triggered() {
+    async fn intransitive_action_type_can_be_defined_and_triggered() {
         let actor = TestActor1;
-        let intransitivie_action: ActionType<TestActor1, DummyActionReceiver> =
-            ActionType::Intransitive {
-                performer: Box::new(actor),
-                action: Arc::new(|actor| Box::pin(proxy(actor))),
-            };
+        let intransitivie_action: Action<TestActor1, DummyActionReceiver> = Action::Intransitive {
+            performer: Box::new(actor),
+            action: Arc::new(|actor| Box::pin(proxy_for_intransitive_action(actor))),
+        };
+
+        execute_action(intransitivie_action).await.unwrap();
     }
 
-    async fn proxy(test_actor_1: Box<TestActor1>) -> Result<(), Box<dyn Error>> {
+    async fn proxy_for_intransitive_action(
+        test_actor_1: Box<TestActor1>,
+    ) -> Result<(), Box<dyn Error>> {
         Ok(test_actor_1
             .do_something_on_its_own()
             .await
             .map_err(|e| Box::new(e))?)
     }
 
-    struct TestStruct {
-        action: Arc<
-            dyn Fn(
-                Box<String>,
-                Box<String>,
-            )
-                -> Pin<Box<dyn Future<Output = Result<(), Box<dyn Error>>> + Send + 'static>>,
-            // dyn Fn() -> Pin<Box<dyn Future<Output = Result<(), Box<dyn Error>>> + Send + 'static>>,
-            // dyn Fn() -> Pin<Box<dyn Future<Output = ()> + Send + 'static>>,
-        >,
-    }
-
-    #[derive(Debug, thiserror::Error)]
-    #[error("asdf")]
-    struct MyError;
-
     #[tokio::test]
-    async fn action_can_be_constructed() {
-        let my_actor_1 = Box::new(String::from("a"));
-        let my_actor_2 = Box::new(String::from("b"));
-
-        // let f: &dyn Fn(Box<dyn ActorBase>, Box<dyn ActorBase>) -> Result<(), Box<dyn Error>> =
-        //     async |my_actor_1: Box<MyActor1>, my_actor_2: Box<MyActor2>| {
-        //         my_actor_1.do_something(my_actor_2).await
-        //     };
-
-        let s = TestStruct {
-            action: Arc::new(|s1, s2| Box::pin(f(s1, s2))),
+    async fn transitive_action_type_can_be_defined_and_triggered() {
+        let actor_1 = TestActor1;
+        let actor_2 = TestActor2;
+        let transitivie_action: Action<TestActor1, TestActor2> = Action::Transitive {
+            performer: Box::new(actor_1),
+            receiver: Box::new(actor_2),
+            action: Arc::new(|actor_1, actor_2| {
+                Box::pin(proxy_for_transitive_action(actor_1, actor_2))
+            }),
         };
 
-        (s.action)(my_actor_1, my_actor_2).await;
+        execute_action(transitivie_action).await.unwrap();
     }
 
-    // fn new_struct<F, Fut>(f: F) -> TestStruct
-    // where
-    //     F: Fn() -> Fut + Send + 'static,
-    //     Fut: Future<Output = ()> + Send + 'static,
-    // {
-    //     TestStruct {
-    //         action: Arc::new(move || Box::pin(f())),
-    //     }
-    // }
+    async fn proxy_for_transitive_action(
+        test_actor_1: Box<TestActor1>,
+        test_actor_2: Box<TestActor2>,
+    ) -> Result<(), Box<dyn Error>> {
+        Ok(test_actor_1
+            .do_something(test_actor_2)
+            .await
+            .map_err(|e| Box::new(e))?)
+    }
 
-    async fn f(s1: Box<String>, s2: Box<String>) -> Result<(), Box<dyn Error>> {
-        println!("{s1}");
-        println!("{s2}");
-        Ok(())
+    async fn execute_action<T1, T2>(action: Action<T1, T2>) -> Result<(), Box<dyn Error>>
+    where
+        T1: ActorBase,
+        T2: ActorBase,
+    {
+        match action {
+            Action::Intransitive { performer, action } => action(performer).await,
+            Action::Transitive {
+                performer,
+                receiver,
+                action,
+            } => action(performer, receiver).await,
+        }
     }
-    async fn f1() -> Result<(), Box<dyn Error>> {
-        // Err(Box::new(MyError))
-        Err(MyError)?
-    }
-    async fn f2() {}
 }
--- core/src/test_utils/test_actor_states.rs
+++ core/src/test_utils/test_actor_states.rs
@@ -16,15 +16,19 @@ impl ActorState for MyActorState2 {}
 
 pub struct TestActor1;
 pub struct TestActor2;
+pub struct TestActor3;
 
 impl ActorBase for TestActor1 {}
 impl ActorBase for TestActor2 {}
+impl ActorBase for TestActor3 {}
 
 impl TestActor1 {
     pub async fn do_something(&self, _: Box<TestActor2>) -> Result<(), TestActor1Error> {
+        println!("test_actor_1.do_something(test_actor_2) ...");
         Ok(())
     }
     pub async fn do_something_on_its_own(&self) -> Result<(), TestActor1Error> {
+        println!("test_actor_1.do_something_on_its_own() ...");
         Ok(())
     }
 }
